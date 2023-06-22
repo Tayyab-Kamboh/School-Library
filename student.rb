6f12b575d23351f7eb6d370b541e7f357f94f28e
Tayyab-Kamboh <tayyabkambohSK28@gmail.com>
2023-06-22 03:04:17 +0200

    Fix linter issues

--- student.rb
+++ student.rb
@@ -2,14 +2,14 @@ require_relative 'person'
 
 class Student < Person
   attr_reader :classroom, :parent_permission
-  attr_accessor :name, :classroom
+  attr_accessor :name, :classroom # rubocop:todo Lint/DuplicateMethods
 
-  def initialize(name)
+  def initialize(name) # rubocop:todo Lint/MissingSuper
     @name = name
     @classroom = nil
   end
 
-  def initialize(age, name = 'unknown', parent_permission: true, classroom: nil)
+  def initialize(age, name = 'unknown', parent_permission: true, classroom: nil) # rubocop:todo Lint/DuplicateMethods
     super(name, age, parent_permission: parent_permission)
     @classroom = classroom
   end
