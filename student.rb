914b130663d74e0c6c04ca623e8ab1fa25622b43
Tayyab-Kamboh <tayyabkambohSK28@gmail.com>
2023-06-22 02:56:38 +0200

    Make required changes

--- student.rb
+++ student.rb
@@ -2,6 +2,11 @@ require_relative 'person'
 
 class Student < Person
   attr_reader :classroom, :parent_permission
+  attr_accessor :name, :classroom
+
+  def initialize(name)
+     @name = name
+     @classroom = nil
 
   def initialize(age, name = 'unknown', parent_permission: true, classroom: nil)
     super(name, age, parent_permission: parent_permission)
